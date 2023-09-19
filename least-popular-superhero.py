39a2e5ecda1e28ef55778bab5e5cb9c820590f1b
MarinKabic <131856660+MarinKabic@users.noreply.github.com>
2023-09-19 21:40:32 +0200

    Update least-popular-superhero.py

--- least-popular-superhero.py
+++ least-popular-superhero.py
@@ -2,36 +2,34 @@ from pyspark.sql import SparkSession
 from pyspark.sql import functions as func 
 from pyspark.sql.types import StructType, StructField, IntegerType, StringType
 
+# Creating a Spark Session
 spark = SparkSession.builder.appName("least_Popular_superhero").getOrCreate()
 
+# Defining the schema for the dataset
 shema = StructType ([\
                      StructField("id", IntegerType(), True),\
                      StructField("hero_name", StringType(), True)])
-    
-names = spark.read.schema(shema).option("sep", " ").csv("file:///SparkCourse/Marvel-Names.txt")
 
-lines = spark.read.text("file:///SparkCourse/Marvel-Graph.txt")
+# Loading the data with ID and Names
+names = spark.read.schema(shema).option("sep", " ").csv("file:///SparkProjects/Marvel-Names.txt")
+lines = spark.read.text("file:///SparkProjects/Marvel-Graph.txt")
 
+# Establishing the connections between superheroes
 numb_connections = lines.withColumn("id", func.split(func.trim(func.col("value")), " ")[0])\
               .withColumn("connections", func.size(func.split(func.trim(func.col("value")), " "))-1)\
               .groupBy("id").agg(func.sum("connections").alias("connections"))
 
-minConnectionCount = numb_connections.agg(func.min("connections")).first()[0]
-               #func.min   - pronadi najmanji broj u stupcu connections
-               #first()    - trebala bi biti samo jedna minimalna vrijednost,
-               #             npr broj 1 samo, a ne 1 i 2
-               #[0]        - da rezultat bude samo 1 stupac, i to taj min(connections)
-               
+# Determining the minimal number of connections
+minConnectionCount = numb_connections.agg(func.min("connections")).first()[0]    
+
 minConnections = numb_connections.filter(func.col("connections") == minConnectionCount)
-               # filter(zagrada)  - da je broj connectionsa iz numb_conn df jednak df-u minConnCount
 
-# join dataframes
+# Joining the dataframes
 minConnectionsWithNames = minConnections.join(names, "id")
-               # UVIJEK PRVO FILTRIRAJ PRIJE JOINANJA 
-               # joinaj dataframe NAMES dataframeu minConnections po ID-ju
 
+# Showing the results
 print("The following characters have only " + str(minConnectionCount) + " connection(s)")
-
 minConnectionsWithNames.select("hero_name").show()
 
+# stop the spark session
 spark.stop()
